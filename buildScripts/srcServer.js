6a5f94aea50c1a2ddc61984195173e786eca4a36
Venkat Subramanian <venkasu2@cisco.com>
2018-04-07 21:58:07 +0530

    intermediate checkin 2

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,7 @@ var open = require('open');
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console */
 const port = 3000;  //ES6 syntax
 var app = express();
 const compiler = webpack(config);
@@ -12,6 +13,15 @@ app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
+
+app.use('/users', function(req,res) {
+  // Hard coding for simplicity.  Pretend it hits the read database
+  res.json([
+    {"id": 1, "firstName":"Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstName":"Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com"},
+    {"id": 3, "firstName":"Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com"}
+  ]);
+});
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
